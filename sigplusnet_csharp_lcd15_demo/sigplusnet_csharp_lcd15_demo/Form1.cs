fe7a91aef2a17a93f5be2376753e7c2ea4f1c92c
Sam Harris <sharris@practicevelocity.com>
2017-08-07 09:27:01 -0500

    backup 9:30

--- sigplusnet_csharp_lcd15_demo/sigplusnet_csharp_lcd15_demo/Form1.cs
+++ sigplusnet_csharp_lcd15_demo/sigplusnet_csharp_lcd15_demo/Form1.cs
@@ -1,17 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Drawing;
-using System.Text;
 using System.Windows.Forms;
-using System.Windows;
-using System.Media;
-using System.Windows.Media.Imaging;
 using System.Diagnostics;
 using System.IO;
+using System.IO.Ports;
 using System.Threading;
-using System.Windows.Forms.VisualStyles;
-using System.Linq;
 using Application = System.Windows.Forms.Application;
 
 
@@ -19,10 +12,8 @@ namespace sigplusnet_csharp_lcd15_demo
 {
    public partial class Form1 : Form
    {
-      Bitmap sign, ok, clear, please;
-      int lcdX, lcdY;
-      uint lcdSize;
-      string imgFile;
+       private Bitmap _sign, _ok, _clear, _please;
+       private string _imgFile;
 
       public Form1()
       {
@@ -31,117 +22,124 @@ namespace sigplusnet_csharp_lcd15_demo
 
       private void cmdStart_Click(object sender, EventArgs e)
       {
-         //The following code will write BMP images out to the LCD 1X5 screen
+         //BMP images to the LCD screen
+         _sign = new Bitmap(Application.StartupPath + "\\Sign.bmp");
+         _ok = new Bitmap(Application.StartupPath + "\\OK.bmp");
+         _clear = new Bitmap(Application.StartupPath + "\\CLEAR.bmp");
+         _please = new Bitmap(Application.StartupPath + "\\please.bmp");
 
-         sign = new System.Drawing.Bitmap(Application.StartupPath + "\\Sign.bmp");
-         ok = new System.Drawing.Bitmap(Application.StartupPath + "\\OK.bmp");
-         clear = new System.Drawing.Bitmap(Application.StartupPath + "\\CLEAR.bmp");
-         please = new System.Drawing.Bitmap(Application.StartupPath + "\\please.bmp");
+        //Determines port to use for signature pad
+          string[] ports = SerialPort.GetPortNames();
+          for (int i = 1; i < ports.Length; i++)
+          {
+              if (sigPlusNET1.IsAccessible)
+              {
+                 sigPlusNET1.SetTabletComPort(i);
+                 break;
+              }
+          }
 
-         sigPlusNET1.SetTabletComPort(3);
-         sigPlusNET1.SetTabletState(1); //Turns tablet on to collect signature
+         sigPlusNET1.SetTabletState(1); //Turns tablet on
          sigPlusNET1.LCDRefresh(0, 0, 0, 240, 64);
          sigPlusNET1.SetTranslateBitmapEnable(false);
 
          //Images sent to the background
-         sigPlusNET1.LCDSendGraphic(1, 2, 0, 20, sign);
-         sigPlusNET1.LCDSendGraphic(1, 2, 207, 4, ok);
-         sigPlusNET1.LCDSendGraphic(1, 2, 15, 4, clear);
+         sigPlusNET1.LCDSendGraphic(1, 2, 0, 20, _sign);
+         sigPlusNET1.LCDSendGraphic(1, 2, 207, 4, _ok);
+         sigPlusNET1.LCDSendGraphic(1, 2, 15, 4, _clear);
 
          //Get LCD size in pixels.
-         lcdSize = sigPlusNET1.LCDGetLCDSize();
-         lcdX = (int)(lcdSize & 0xFFFF);
-         lcdY = (int)((lcdSize >> 16) & 0xFFFF);
+         sigPlusNET1.LCDGetLCDSize();
 
-         Font f = new System.Drawing.Font("Arial", 9.0F, System.Drawing.FontStyle.Regular);
+         new Font("Arial", 9.0F, FontStyle.Regular);
 
          sigPlusNET1.ClearTablet();
 
          sigPlusNET1.LCDSetWindow(0, 0, 1, 1);
-         sigPlusNET1.SetSigWindow(1, 0, 0, 1, 1); //Sets the area where ink is permitted in the SigPlus object
+         sigPlusNET1.SetSigWindow(1, 0, 0, 1, 1); //Sets the signing area
          sigPlusNET1.SetLCDCaptureMode(2);   //Sets mode so ink will not disappear after a few seconds
 
 
          //Shows signature window with clear and ok buttons
 
          sigPlusNET1.ClearSigWindow(1);
-         sigPlusNET1.LCDRefresh(1, 16, 45, 50, 15); //Refresh LCD at 'Continue' to indicate to user that this option has been sucessfully chosen
-         sigPlusNET1.LCDRefresh(2, 0, 0, 240, 64); //Brings the background image already loaded into foreground
+         sigPlusNET1.LCDRefresh(1, 16, 45, 50, 15); //Refresh LCD to indicate that the option has been sucessfully chosen
+         sigPlusNET1.LCDRefresh(2, 0, 0, 240, 64); //Brings background image into foreground
          sigPlusNET1.ClearTablet();
          sigPlusNET1.KeyPadClearHotSpotList();
-         sigPlusNET1.KeyPadAddHotSpot(2, 1, 10, 5, 53, 17); //For CLEAR button
-         sigPlusNET1.KeyPadAddHotSpot(3, 1, 197, 5, 19, 17); //For OK button
+         sigPlusNET1.KeyPadAddHotSpot(2, 1, 10, 5, 53, 17); //CLEAR button
+         sigPlusNET1.KeyPadAddHotSpot(3, 1, 197, 5, 19, 17); //OK button
          sigPlusNET1.LCDSetWindow(2, 22, 236, 40);
-         sigPlusNET1.SetSigWindow(1, 0, 22, 240, 40); //Sets the area where ink is permitted in the SigPlus object
+         sigPlusNET1.SetSigWindow(1, 0, 22, 240, 40); //Sets the signing area
       }
       private void sigPlusNET1_PenDown(object sender, EventArgs e)
       {
       }
       private void sigPlusNET1_PenUp(object sender, EventArgs e)
       {
-         string strSig;
-         sigPlusNET1.SetLCDCaptureMode(2);
+          sigPlusNET1.SetLCDCaptureMode(2);
 
-         if (sigPlusNET1.KeyPadQueryHotSpot(2) > 0) //If the CLEAR hotspot is tapped, then...
+         if (sigPlusNET1.KeyPadQueryHotSpot(2) > 0) //If CLEAR hotspot is tapped...
          {
             sigPlusNET1.ClearSigWindow(1);
-            sigPlusNET1.LCDRefresh(1, 10, 0, 53, 17); //Refresh LCD at 'CLEAR' to indicate to user that this option has been sucessfully chosen
-            sigPlusNET1.LCDRefresh(2, 0, 0, 240, 64); //Brings the background image already loaded into foreground
+            sigPlusNET1.LCDRefresh(1, 10, 0, 53, 17); //Refresh LCD
+            sigPlusNET1.LCDRefresh(2, 0, 0, 240, 64); //Brings background image into foreground
             sigPlusNET1.ClearTablet();
          }
-         else if (sigPlusNET1.KeyPadQueryHotSpot(3) > 0) //If the OK hotspot is tapped, then...
+         else if (sigPlusNET1.KeyPadQueryHotSpot(3) > 0) //If OK hotspot is tapped...
          {
             sigPlusNET1.ClearSigWindow(1);
 
-            strSig = sigPlusNET1.GetSigString();
-            sigPlusNET1.LCDRefresh(1, 210, 3, 14, 14); //Refresh LCD at 'OK' to indicate to user that this option has been sucessfully chosen
+            sigPlusNET1.GetSigString();
+            sigPlusNET1.LCDRefresh(1, 210, 3, 14, 14); //Refresh LCD
 
             if (sigPlusNET1.NumberOfTabletPoints() > 0)
             {
                sigPlusNET1.LCDRefresh(0, 0, 0, 240, 64);
-               Font f = new System.Drawing.Font("Arial", 9.0F, System.Drawing.FontStyle.Regular);
+               var f = new Font("Arial", 9.0F, FontStyle.Regular);
 
                sigPlusNET1.SetImageXSize(500);
                sigPlusNET1.SetImageYSize(100);
-               System.Drawing.Image myImg = sigPlusNET1.GetSigImage();
+               var myImg = sigPlusNET1.GetSigImage();
 
 
                //Opens save file dialog
-               SaveFileDialog saveFileDialog1 = new SaveFileDialog();
-               saveFileDialog1.Filter = "Bitmap Image|*.bmp|GIF Image|*.Gif|JPeg Image|*.jpg|PNG Image|*.Png";
-               saveFileDialog1.Title = "Save Signature Image";
-               saveFileDialog1.ShowDialog();
+                var saveFileDialog1 =
+                    new SaveFileDialog
+                    {
+                        Filter = @"Bitmap Image|*.bmp|GIF Image|*.Gif|JPeg Image|*.jpg|PNG Image|*.Png",
+                        Title = @"Save Signature Image"
+                    };
+                saveFileDialog1.ShowDialog();
 
                if (saveFileDialog1.FileName != "")
                {
-                  System.IO.FileStream fs = (System.IO.FileStream)saveFileDialog1.OpenFile();
+                  var fs = (FileStream)saveFileDialog1.OpenFile();
 
-                  switch (saveFileDialog1.FilterIndex)
-                  {
-                     case 1:
-                        myImg.Save(fs,System.Drawing.Imaging.ImageFormat.Bmp);
-                        imgFile = saveFileDialog1.FileName;
-                        break;
-
-                     case 2:
-                        myImg.Save(fs,System.Drawing.Imaging.ImageFormat.Gif);
-                        imgFile = saveFileDialog1.FileName; 
-                        break;
-
-                     case 3:
-                        myImg.Save(fs,System.Drawing.Imaging.ImageFormat.Jpeg);
-                        imgFile = saveFileDialog1.FileName; 
-                        break;
-
-                     case 4:
-                        myImg.Save(fs, System.Drawing.Imaging.ImageFormat.Png);
-                        imgFile = saveFileDialog1.FileName;
-                        break;
-                  }
-                  fs.Close();
+                   if (saveFileDialog1.FilterIndex == 1)
+                   {
+                       myImg.Save(fs, System.Drawing.Imaging.ImageFormat.Bmp);
+                       _imgFile = saveFileDialog1.FileName;
+                   }
+                   else if (saveFileDialog1.FilterIndex == 2)
+                   {
+                       myImg.Save(fs, System.Drawing.Imaging.ImageFormat.Gif);
+                       _imgFile = saveFileDialog1.FileName;
+                   }
+                   else if (saveFileDialog1.FilterIndex == 3)
+                   {
+                       myImg.Save(fs, System.Drawing.Imaging.ImageFormat.Jpeg);
+                       _imgFile = saveFileDialog1.FileName;
+                   }
+                   else if (saveFileDialog1.FilterIndex == 4)
+                   {
+                       myImg.Save(fs, System.Drawing.Imaging.ImageFormat.Png);
+                       _imgFile = saveFileDialog1.FileName;
+                   }
+                   fs.Close();
                }
-               this.WindowState = FormWindowState.Minimized;
-               Process.Start(imgFile);
+               WindowState = FormWindowState.Minimized;
+               Process.Start(_imgFile);
                sigPlusNET1.LCDWriteString(0, 2, 35, 25, f, "Signature capture complete.");
                Thread.Sleep(1000);
                Application.Exit();
@@ -149,7 +147,7 @@ namespace sigplusnet_csharp_lcd15_demo
             else
             {
                sigPlusNET1.LCDRefresh(0, 0, 0, 240, 64);
-               sigPlusNET1.LCDSendGraphic(0, 2, 4, 20, please);
+               sigPlusNET1.LCDSendGraphic(0, 2, 4, 20, _please);
                sigPlusNET1.ClearTablet();
                sigPlusNET1.LCDRefresh(2, 0, 0, 240, 64);
                sigPlusNET1.SetLCDCaptureMode(2);   //Sets mode so ink will not disappear after a few seconds
@@ -159,13 +157,13 @@ namespace sigplusnet_csharp_lcd15_demo
       }
       private void Form1_FormClosing(object sender, FormClosingEventArgs e)
       {
-         //reset hardware
+         //reset LCD
          sigPlusNET1.LCDRefresh(0, 0, 0, 240, 64);
          sigPlusNET1.LCDSetWindow(0, 0, 240, 64);
          sigPlusNET1.SetSigWindow(1, 0, 0, 240, 64);
          sigPlusNET1.KeyPadClearHotSpotList();
 
-         Bitmap blank = new System.Drawing.Bitmap(240, 64);
+         var blank = new Bitmap(240, 64);
          sigPlusNET1.LCDSendGraphic(1, 0, 0, 0, blank);
 
          sigPlusNET1.SetLCDCaptureMode(1);
--- sigplusnet_csharp_lcd15_demo/sigplusnet_csharp_lcd15_demo/Program.cs
+++ sigplusnet_csharp_lcd15_demo/sigplusnet_csharp_lcd15_demo/Program.cs
@@ -1,16 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace sigplusnet_csharp_lcd15_demo
 {
-    static class Program
+    internal static class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        private static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
